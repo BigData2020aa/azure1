9ac7fa90cbefc0122ec897eabbccd249be43ce17
BigData2020aa <65924034+BigData2020aa@users.noreply.github.com>
2021-11-29 23:02:14 +0300

    Add files via upload

--- odr.js
+++ odr.js
@@ -140,10 +140,10 @@ window.addEventListener('load', function() {
         arg.Inputs.input1.Values.push(p);
 
         jQuery.ajax({
-                url: "https://ocrv1.azure-api.net/predict/score",
+                url: "https://cloudresource.azure-api.net/predict/score",
                 beforeSend: function(xhrObj) {
                     xhrObj.setRequestHeader("Content-Type", "application/json;charset=utf-8");
-                    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "cddeea094118480f8f5e99b460429ea3");
+                    xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key", "cf20287b2dcb4f3bac67346099cb5a23");
 
                 },
                 type: "POST",
